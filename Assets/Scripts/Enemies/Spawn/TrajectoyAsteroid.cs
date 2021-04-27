33c1766696e3f9d3a89edc0b9033778853552ac4
Berry-Jam <marco07091990@gmail.com>
2021-04-27 11:59:28 +0100

    First commit

--- Assets/Scripts/Enemies/Spawn/TrajectoyAsteroid.cs
+++ Assets/Scripts/Enemies/Spawn/TrajectoyAsteroid.cs
@@ -6,20 +6,15 @@ public class TrajectoyAsteroid : MonoBehaviour
 {
     public float Speed;
     public GameObject Target;
-    Transform direction;
+    Vector3 direction;
 
-
-    void start(){
-        getDirection();
-    }
+    void start1(){
+      direction = new Vector3(Random.Range(-3.0f,3.0f),Random.Range(-4.0f,0f),0).normalized; //random position in x and y
+          }
 
     // Update is called once per frame
-    void Update()
-    {
-    transform.position += direction * Speed * Time.deltaTime;
+    void Update(){
+            transform.position +=  direction * Speed * Time.deltaTime;
     }
-    void getDirection(){
-      direction += (transform.position - Target.transform.position).normalized;
-      }
 
 }
